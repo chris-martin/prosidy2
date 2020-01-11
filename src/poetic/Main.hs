9c1cf799469e75945a44ddefc871d2ccbb499478
Alex Feldman-Crough <alex@fldcr.com>
2020-01-10 17:57:22 -0800

    Clean up poetic

--- src/poetic/Main.hs
+++ src/poetic/Main.hs
@@ -1,23 +1,97 @@
+{-# LANGUAGE ApplicativeDo #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE RecordWildCards #-}
 module Main where
 
-import           Prosidy
+import           Prosidy hiding (NonEmpty, nonEmpty)
+import Data.List.NonEmpty (NonEmpty, nonEmpty)
 import           Control.Lens
-import           System.Environment             ( getArgs )
 import qualified Data.Text.IO                  as Text.IO
+import qualified Options.Applicative as A
+import qualified Options.Applicative.Builder as AB
+import qualified System.IO as IO
+import Data.Text (Text)
+import qualified Data.Text as Text
+import Control.Applicative (Alternative(..), optional)
+import qualified Data.HashSet as HashSet
 
 main :: IO ()
 main = do
-    args <- getArgs
+    options  <- getOptions
+    document <- readDocument $ inputPath options
+    let lines = document ^.. allBlocks . _BlockLiteral 
+                           . filtered (optionsFilter options) 
+                           . content . _Literal
+    Text.IO.writeFile (outputPath options) . Text.unlines $
+        optionsPrefix options lines
+  where
+      allBlocks = cosmosOnOf (content . folded) (_BlockTag . content . folded)
 
-    let [_, input, output] = filter ((/= "-") . take 1) args
-    doc <- readDocument input
-    doc
-        ^. cosmosOnOf (content . folded) (_BlockTag . content . folded)
-        .  _BlockLiteral
-        .  filtered (has $ tag . only [keyQ|haskell|])
-        .  content
-        .  coerced
-        .  to (<> "\n")
-        &  Text.IO.writeFile output
+data Options = Options
+   { sourceLabel :: Maybe Text
+   , tagFilter   :: Maybe (NonEmpty Key)
+   , inputPath   :: FilePath
+   , outputPath  :: FilePath
+   } deriving (Show)
+
+getOptions :: IO Options
+getOptions = A.execParser $ A.info parse info
+  where
+    parse :: A.Parser Options
+    parse = do
+        AB.abortOption A.ShowHelpText $ mconcat 
+            [ A.long "help"
+            , A.help "Show this help text"
+            , A.hidden
+            ]
+        tagFilter <- fmap nonEmpty . many . A.option readKey $ mconcat
+            [ A.short 't'
+            , A.long "tag"
+            , A.help "Extract only literals matching this tag."
+            , A.metavar "TAG"
+            ]
+        sourceLabel <- optional . A.strOption $ mconcat
+            [ A.short 'h'
+            , A.long "label"
+            , A.help "The name of the file that should be displayed to the user"
+            , A.metavar "LABEL"
+            ]
+        inputPath <- A.strArgument $ mconcat
+            [ A.help "The actual location of the literate file to process"
+            , A.metavar "INPUT"
+            ]
+        outputPath <- A.strArgument $ mconcat
+            [ A.help "The location where the extract source will be written"
+            , A.metavar "OUTPUT"
+            ]
+        pure Options{..}
+
+    info :: A.InfoMod Options
+    info = mconcat
+        [ A.progDesc "A program to extract source code from literate\
+                     \ programming files written in Prosidy."
+        ]
+
+    readKey :: A.ReadM Key
+    readKey = A.maybeReader (toKey . Text.pack)
+
+optionsPrefix :: Options -> [Text] -> [Text]
+optionsPrefix Options{sourceLabel} =
+  case sourceLabel of
+    Nothing ->
+        id
+    Just lbl ->
+        (:) (mconcat ["#line 1 \"", lbl, "\""])
+
+optionsFilter :: Options -> LiteralTag -> Bool
+optionsFilter Options{tagFilter} =
+  case tagFilter of
+    Nothing ->
+        const True
+    Just tags ->
+      let
+        set     = foldMap HashSet.singleton tags
+        check x = HashSet.member (x ^. tag) set
+      in
+        check
