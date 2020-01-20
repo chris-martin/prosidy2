bfef689abfeaaabb3a2bd8d33befcb30bf744a5f
Alex Feldman-Crough <alex@fldcr.com>
2020-01-20 11:20:10 -0800

    Remove unneeded directories now that make is dead

--- scripts/lib/setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-cd "$(git rev-parse --show-toplevel)"
-msg ()  { printf -- '\e[1m----> %s\e[m\n' "$*" >&2; }
-warn () { printf -- '\e[33;1m----! %s\e[m\n' "$*" >&2; }
-err ()  { printf -- '\e[31;1m----! %s\e[m\n' "$*" >&2; exit 1; }
-
