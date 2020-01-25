54d09547c0c7dbb974d4bb71d700949624a80efb
Alex Feldman-Crough <alex@fldcr.com>
2020-01-25 11:35:43 -0800

    Revert to a nixpkgs release before gcc9 become the default

--- nix/nixpkgs.nix
+++ nix/nixpkgs.nix
@@ -9,8 +9,8 @@ args:
 let
   static     = builtins.getEnv "NIX_STATIC" != "";
   url        = "https://github.com/NixOS/nixpkgs-channels/archive/${commit}.tar.gz";
-  commit     = "05626cc86b8a8bbadae7753d2e33661400ff67de";
-  sha256     = "1lx5hs2yzg21pc8fv982kdqhc5j5kxi08h8wn3plx848bcnd8jj6";
+  commit     = "83a5ad13b743650e9fc5e79409b9e9470602fcf9";
+  sha256     = "0hv6bl0r3gvz7b1mm0hm1bshxcwh7xpvj3x8aks7hfw3axvshh2r";
   nixpkgs    = import tarball args;
   tarball    = builtins.fetchTarball { inherit url sha256; };
 in
